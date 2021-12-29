d599c7e7cb989a36b8923ec96f3875331d195169
nivinabbas <nivin.abbas1@gmail.com>
2021-12-29 14:58:51 +0200

    modify app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Users from "./components/Users";
+import { BrowserRouter as Router,Link, Route} from 'react-router-dom';
 import Catalog from './components/Catalog';
-import NavBar from './components/NavBar';
+import { Component } from 'react';
 
-
-const App = () => {
- 
+class App extends Component{
+ render(){
   return (
     <div className="container-fluid movie-app">
-      <NavBar/>
- 
+       <Router>
+       
+       <div id="main-links">
+       <h3 className='logo'>Reflix</h3>
+           <Link to="/">Users</Link>
+           <Link to="/catalog">Catalog</Link>
+           <Route exact path="/" component={Users} />
+           <Route exact path="/catalog" component={Catalog} />
+     
+         </div>
+       </Router>
     </div>
   );
-};
+}};
 
 export default App;
