78b6cfbbfd8e6611bf2aa758fa00c840dcb007f4
nivinabbas <nivin.abbas1@gmail.com>
2021-12-29 22:26:48 +0200

    convert User functional component to class component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,43 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import Link from 'react-router-dom/NavLink';
 
-const Users = () => {
-  return (
-    <div>
-      <h1 className="header-text">Who's Watching</h1>
-      <div id="home-container">
-       
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="header-text">Who's Watching</h1>
+        <div id="home-container">
           <div className="user">
-          <Link to={'/catalog'}>
-            <span className="main-directory-text">Nivin</span>
+            <Link to={'/catalog'}>
+              <span className="main-directory-text">Nivin</span>
             </Link>
-
           </div>
-        
-        <div className="user">
-        <Link to={'/catalog'}>
-          <span className="main-directory-text">Fadi</span>
-          </Link>
-        </div>
-        <div className="user">
-        <Link to={'/catalog'}>
-          <span className="main-directory-text">Ali</span>
-          </Link>
-        </div>
-        <div className="user">
-        <Link to={'/catalog'}>
-          <span className="main-directory-text">Asad</span>
-          </Link>
-        </div>
-        <div className="user">
-        <Link to={'/catalog'}>
-          <span className="main-directory-text">Nareen</span>
-          </Link>
+
+          <div className="user">
+            <Link to={'/catalog'}>
+              <span className="main-directory-text">Fadi</span>
+            </Link>
+          </div>
+          <div className="user">
+            <Link to={'/catalog'}>
+              <span className="main-directory-text">Ali</span>
+            </Link>
+          </div>
+          <div className="user">
+            <Link to={'/catalog'}>
+              <span className="main-directory-text">Asad</span>
+            </Link>
+          </div>
+          <div className="user">
+            <Link to={'/catalog'}>
+              <span className="main-directory-text">Nareen</span>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
