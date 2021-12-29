c24df551c9ccb77aee01c28f76e7189458288714
nivinabbas <nivin.abbas1@gmail.com>
2021-12-29 02:09:22 +0200

    add search box input

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm">
+      <input className='form-control' placeholder='type to search' value={props.value} onChange={(event)=> props.setSearchValue(event.target.value)}></input>
+    </div>
+  );
+};
+
+export default SearchBox;
