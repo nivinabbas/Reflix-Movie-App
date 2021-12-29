3ac68bd273f554e8092018e853ab9d99b5eb5acd
nivinabbas <nivin.abbas1@gmail.com>
2021-12-29 14:58:39 +0200

    modify route links

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import Link from 'react-router-dom/NavLink';
 
+const Users = () => {
+  return (
+    <div>
+      <h1 className="header-text">Who's Watching</h1>
+      <div id="home-container">
+       
+          <div className="user">
+          <Link to={'/catalog'}>
+            <span className="main-directory-text">Nivin</span>
+            </Link>
 
-
-const Users =()=> {
-
-        return (
-            <div >
-                <h1 className='header-text' >Who's Watching</h1>
-                <div id="home-container">
-          
-                  
-                 <div className="user" ><a href='/catalog' className="main-directory-text">Nivin</a></div>
-                <div className="user"><a href='/catalog' className="main-directory-text">Fadi</a></div>
-                <div className="user"><a href='/catalog' className="main-directory-text">Ali</a></div>
-                <div className="user"><a href='/catalog' className="main-directory-text">Asad</a></div>
-                <div className="user"><a href='/catalog' className="main-directory-text">Nareen</a></div>
-          
-                  
-            </div>
-
-            </div>
-     
-        );
-    }
-
+          </div>
+        
+        <div className="user">
+        <Link to={'/catalog'}>
+          <span className="main-directory-text">Fadi</span>
+          </Link>
+        </div>
+        <div className="user">
+        <Link to={'/catalog'}>
+          <span className="main-directory-text">Ali</span>
+          </Link>
+        </div>
+        <div className="user">
+        <Link to={'/catalog'}>
+          <span className="main-directory-text">Asad</span>
+          </Link>
+        </div>
+        <div className="user">
+        <Link to={'/catalog'}>
+          <span className="main-directory-text">Nareen</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Users;
