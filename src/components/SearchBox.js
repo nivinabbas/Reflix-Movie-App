955476ae1b852815256263c2ab55aa94ca58c19a
nivinabbas <nivin.abbas1@gmail.com>
2021-12-29 23:23:41 +0200

    refactor: change searchBox  to class component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBox = (props) => {
+class SearchBox extends Component{
+  render(){
+    const {value,setSearchValue}=this.props;
   return (
     <div className="col col-sm">
-      <input className='form-control' placeholder='type to search' value={props.value} onChange={(event)=> props.setSearchValue(event.target.value)}></input>
+      <input
+        className="form-control"
+        placeholder="type to search"
+        value={value}
+        onChange={(event) => setSearchValue(event.target.value)}
+      ></input>
     </div>
   );
-};
+}};
 
 export default SearchBox;
